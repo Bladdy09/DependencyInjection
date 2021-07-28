6e271db9599a8d43c5c1acd2d7aef5015fbf43fa
HP <HP@DESKTOP-QILN7PV>
2021-07-27 23:40:58 -0400

    Agregar archivos de proyecto.

---
+++ Inyecciones/Models/Region.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Inyecciones.Models
+{
+    public partial class Region
+    {
+        public Region()
+        {
+            Territories = new HashSet<Territory>();
+        }
+
+        [Required(ErrorMessage = "favor de digital el numero de identifiacion de la region")]
+
+        public int RegionId { get; set; }
+        [Required(ErrorMessage ="Por favor llenar la descripcion")]
+        public string RegionDescription { get; set; }
+
+        public virtual ICollection<Territory> Territories { get; set; }
+    }
+}
